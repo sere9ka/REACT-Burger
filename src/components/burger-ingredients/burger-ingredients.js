a6c334f85e6fd8c7369d51e7cac43d58f0f4664c
sere9ka <s.kovylov@zhiet.ru>
2022-03-24 11:02:59 +0300

    дописал ингредиенты

--- src/components/burger-ingredients/burger-ingredients.js
+++ src/components/burger-ingredients/burger-ingredients.js
@@ -1,25 +1,68 @@
 import React from 'react'
 import ingredientStyles from './burger-ingredients.module.css'
-import { Logo, BurgerIcon, ListIcon, ProfileIcon} from '@ya.praktikum/react-developer-burger-ui-components'
+import { Tab, CurrencyIcon, Counter } from '@ya.praktikum/react-developer-burger-ui-components'
 
+const Ingredient = ({ingredient}) => {
+    return (
+        <>
+            <Counter count={1} size="default"  className={`${ingredientStyles.counter}`} />
+            <div className={`${ingredientStyles.cardImg}`}>
+                <img className={`${ingredientStyles.img}`} src={ingredient.image} alt="картинка" />
+            </div>
+            <div className={`${ingredientStyles.cardDesc} mt-1 pb-8`}>
+                <h3 className={`${ingredientStyles.cardTitle} text text_type_digits-default mb-1`}> <span className={`mr-2`}>{ingredient.price}</span> <CurrencyIcon /></h3>
+                <span className={`${ingredientStyles.cardName} text text_type_main-default`}>{ingredient.name}</span>
+            </div>  
+        </>
+    )
+}
 
 const GetIngredient = ({ingredients}) => {
     console.log(ingredients)
-   return (
-    <>
-        { ingredients.map(ingredient => (
-            <div key={ingredient._id}>{ingredient.name}</div>
-        ))}
-    </>
+
+    const [current, setCurrent] = React.useState('one')
+    return (
+        <>
+            <div style={{ display: 'flex' }}>
+                <Tab value="Булки" to="bun" active={current === 'Булки'} onClick={setCurrent}>
+                    Булки
+                </Tab>
+                <Tab value="Соусы" to="main" active={current === 'Соусы'} onClick={setCurrent}>
+                    Соусы
+                </Tab>
+                <Tab value="Начинки" to="sauce" active={current === 'Начинки'} onClick={setCurrent}>
+                    Начинки
+                </Tab>
+            </div>
+            <div className={`${ingredientStyles.listIngredients} mt-10`}>
+                <h3 className={`text text_type_main-small mb-6 ${ingredientStyles.menuTitle}`}>Булки</h3>
+                    { ingredients.filter(item => item.type === 'bun').map(ingredient => (
+                        <div className={`${ingredientStyles.card} mb-10`} id="bun" key={ingredient._id}>
+                            <Ingredient ingredient={ingredient} />
+                        </div>
+                    ))}
+                <h3 className={`text text_type_main-small mb-6 ${ingredientStyles.menuTitle}`}>Соусы</h3>
+                    { ingredients.filter(item => item.type === 'sauce').map(ingredient => (
+                        <div className={`${ingredientStyles.card} mb-10`} id="bun" key={ingredient._id}>
+                            <Ingredient ingredient={ingredient} />
+                        </div>
+                    ))}
+                <h3 className={`text text_type_main-small mb-6 ${ingredientStyles.menuTitle}`}>Наполнители</h3>
+                    { ingredients.filter(item => item.type === 'main').map(ingredient => (
+                        <div className={`${ingredientStyles.card} mb-10`} id="bun" key={ingredient._id}>
+                            <Ingredient ingredient={ingredient} />
+                        </div>
+                    ))}
+            </div>
+        </>
    )
 }
 
 const BurgerIngredients = (props) => {
-    // console.log(props.ingredients);
     const ingredients = props.ingredients
     return (
        <section className={ingredientStyles.section}>
-           <h2 className={`text text_type_main-large`}>Соберите бургер</h2>
+           <h2 className={`text text_type_main-large mt-10 mb-5`}>Соберите бургер</h2>
             <GetIngredient ingredients ={ingredients} />
        </section>
     )
